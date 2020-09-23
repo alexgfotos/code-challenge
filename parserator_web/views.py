f6ffd6f506bc59337f3b6e7523adc95e244c889e
alex g <alexgfotos@gmail.com>
2020-09-22 18:23:36 -0700

    basic api funcionailty implemented

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
+from django.http import JsonResponse
+
+addr = "215 N norton ave tucson az 85719"
+print(usaddress.tag(addr))
 
 
 class Home(TemplateView):
@@ -13,23 +17,26 @@ class Home(TemplateView):
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
-    def get(self, request):
-        
-        if request.method == 'GET':
-            r = requests.get('api/parse/', params=request.POST)
-        if r.status_code == 200:
-            return Response('Yay, it worked')
-        return Response('Could not save data')
+    def get(self, request): 
+        query = request.GET.get('address')
+        addressComponents = usaddress.parse(query)
+        print(addressComponents)
+        # This will call the parse method when request is sent from the index.html form    
        
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
         # 
-        return Response({})
+        return Response(addressComponents)
 
     def parse(self, address):
-        usaddress.tag(address)
+        
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
         
+        # this should receieve an address from the GET request, parse the address, and return JSON using serializer
+        
+        usaddress.tag(address)
+        #and then serialize?
+
         return address_components, address_type
 
